bcc09fee5a608856911cb48fa50e367278b0c9c2
yzx <nekomail@qq.com>
2018-03-21 23:55:06 +0800

    最初版本

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <el-container>
+
+      <el-header>
+        <el-row>
+          <el-col :span="22">
+            <el-menu :default-active="activeIndex" mode="horizontal" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b"
+              @select="handleSelect">
+              <el-menu-item index="1">成员管理</el-menu-item>
+              <el-menu-item index="2">所有任务</el-menu-item>
+              <el-menu-item index="3">我的任务</el-menu-item>
+              <el-menu-item index="4">任务分析</el-menu-item>
+            </el-menu>
+          </el-col>
+          <el-col :span="2">
+            <el-dropdown>
+              <span style="color:white;">
+                黄小明
+                <i class="el-icon-arrow-down el-icon--right"></i>
+              </span>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>
+                    <i class="el-icon-edit"></i> 修改密码
+                </el-dropdown-item>
+                <el-dropdown-item>
+                    <i class="el-icon-d-arrow-right"></i> 退出登录
+                  </el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+          </el-col>
+        </el-row>
+
+
+      </el-header>
+
+      <router-view/>
+
+    </el-container>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        activeIndex: '1'
+      }
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    color: #2c3e50;
+  }
+
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    height: 100%;
+  }
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,45 @@
+<template>
+    <el-container>
+        <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+
+        </el-aside>
+
+        <el-container>
+            <el-main>
+                <el-table :data="tableData">
+                    <el-table-column prop="date" label="日期" width="140">
+                    </el-table-column>
+                    <el-table-column prop="name" label="姓名" width="120">
+                    </el-table-column>
+                    <el-table-column prop="address" label="地址">
+                    </el-table-column>
+                </el-table>
+            </el-main>
+        </el-container>
+    </el-container>
+</template>
+<script>
+    export default {
+        data() {
+            const item = {
+                date: '2016-05-02',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            };
+            return {
+                tableData: Array(20).fill(item)
+            }
+        }
+    };
+</script>
+<style>
+    .el-header {
+        background-color: #545c64;
+        color: #333;
+        line-height: 60px;
+    }
+
+    .el-aside {
+        color: #333;
+    }
+</style>
